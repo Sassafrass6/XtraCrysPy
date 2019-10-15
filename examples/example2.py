21c7858aa4166afc68443bbd1013cb6360373583
Frank Cerasoli <franklin.cerasoli@unt.edu>
2019-10-15 15:48:05 -0500

    Typo in example2 comments

--- examples/example2.py
+++ examples/example2.py
@@ -2,7 +2,7 @@ from XCrysPy import XCrysPy as XCP
 
 if '__main__' == __name__:
 
-  # Draw the unit cell for Iron with modified background and bond colors.
+  # Draw the unit cell for Iron with modified background and boundary colors.
   #    Tuples formatted as (R,G,B)
   crystal = XCP.XCrysPy(qe_fname='Fe.scf.in', bg_col=(1,1,1), bnd_col=(0,0,0))
   crystal.draw_cell(boundary=True)
