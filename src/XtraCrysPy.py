927357d0ef41350c6de43b81f71d303b51331b84
Frank Cerasoli <franklin.cerasoli@unt.edu>
2022-03-05 17:13:20 -0800

    Fixed extrememly frustrating bug with the coordinate axis alignment

--- src/XtraCrysPy.py
+++ src/XtraCrysPy.py
@@ -3,46 +3,101 @@ import numpy as np
 
 class XtraCrysPy:
 
-  def __init__ ( self, size=(1024, 1024), structure=None, axes=False ):
+  def __init__ ( self, size=(1024, 1024), axes=True, perspective=False ):
     '''
-
     Arguments:
     '''
     from fury import ui,window
 
     self.wsize = size
-    self.aposs = None
-    self.atoms = None
-    self.bonds = None
-    self.natoms = None
     self.picker = None
     self.sel_inds = []
     self.sel_cols = []
     self.stype = 'info'
     self.scolor = np.array((0,210,210))
-    
+
     self.scene = window.Scene()
     self.smanager = window.ShowManager(self.scene, size=size, order_transparent=True)
     self.smanager.initialize()
 
     self.axes = axes
-    self.axis_eles = [None]*3
-    self.axis_lines = [None]*3
-    self.axis_vecs = np.array([[1,0,0],[0,1,0],[0,0,1]])
     if axes:
+      self.axis_eles = [None]*3
+      self.axis_lines = [None]*3
       self.axis_width = ax_wid = 200
       self.ax_panel = ui.Panel2D(size=(ax_wid,ax_wid), color=(0,0,0))
       self.ax_panel.center = (ax_wid/2, ax_wid/2)
       self.scene.add(self.ax_panel)
 
-      centers = self.axis_endpoint_positions()
+      axis_vecs = np.array([[1,0,0],[0,1,0],[0,0,1]])
+      centers = self.axis_endpoint_positions(axis_vecs)
       for i in range(3):
-        self.axis_eles[i] = ui.Disk2D(outer_radius=8, center=centers[i], color=self.axis_vecs[i])
-        self.axis_lines[i] = ui.Line2D((ax_wid/2,ax_wid/2), centers[i], color=self.axis_vecs[i])
+        self.axis_eles[i] = ui.Disk2D(outer_radius=8, center=centers[i], color=axis_vecs[i])
+        self.axis_lines[i] = ui.Line2D((ax_wid/2,ax_wid/2), centers[i], color=axis_vecs[i])
         self.axis_lines[i].width = 5
 
         self.scene.add(self.axis_eles[i])
         self.scene.add(self.axis_lines[i])
+
+
+  def axis_endpoint_positions ( self, vecs ):
+    import numpy as np
+    centers = np.empty((3,3), dtype=float)
+    for i in range(3):
+      wid = self.axis_width / 2
+      centers[i,:] = np.array([wid,wid,0]) + 80*vecs[i]
+    return centers[:,:2]
+
+
+  def left_click ( self, obj, event ):
+    pass
+
+
+  def update_axes ( self, caller, event ):
+
+    camera = self.scene.GetActiveCamera()
+
+    y = np.array(camera.GetViewUp())
+    y /= np.linalg.norm(y)
+    z = np.array(camera.GetFocalPoint()) - np.array(camera.GetPosition())
+    z /= np.linalg.norm(z)
+    x = np.cross(y, z)
+    x /= np.linalg.norm(x)
+
+    rot = []
+    unit = np.eye(3)
+    cam_vecs = np.array([-x,y,z])
+    for i,u in enumerate(unit[:2]):
+      v = np.cross(cam_vecs[i], u)
+      r = np.array([[0,-v[2],v[1]],[v[2],0,-v[0]],[-v[1],v[0],0]])
+      rot.append(np.eye(3) + r + r@r/(1+cam_vecs[i].dot(u)))
+      cam_vecs = rot[-1] @ cam_vecs.T
+
+    axis_vecs = np.eye(3) @ np.linalg.inv(rot[0]) @ np.linalg.inv(rot[1])
+
+    for i,v in enumerate(axis_vecs):
+        axis_vecs[i] /= np.linalg.norm(v)
+    centers = self.axis_endpoint_positions(axis_vecs)
+    axis_order = np.argsort([v[2] for v in axis_vecs])
+    for i,o in enumerate(axis_order):
+      color = np.zeros(3); color[o] = 1
+      self.axis_lines[i].color = color
+      self.axis_lines[i].p2 = centers[o]
+      self.axis_eles[i].color = color
+      self.axis_eles[i].center = centers[o]
+
+
+  def start_crystal_view ( self ):
+    from fury import pick
+
+    self.picker = pick.PickingManager()
+    self.atoms.AddObserver('LeftButtonPressEvent', self.left_click, 1)
+    if self.axes:
+      self.smanager.iren.AddObserver('InteractionEvent', self.update_axes)
+    self.smanager.start()
+
+
+class XCP_Atoms ( XtraCrysPy ):
 
 
   def set_atom_color ( self, mem, index, nvert, col ):
@@ -97,40 +152,6 @@ class XtraCrysPy:
     update_actor(obj)
 
 
-  def axis_endpoint_positions ( self ):
-    centers = np.empty((3,3), dtype=float)
-    for i in range(3):
-      wid = self.axis_width / 2
-      centers[i,:] = np.array([wid,wid,0]) + 80*self.axis_vecs[i]
-    return centers[:,:-1]
-
-
-  def update_axes ( self, caller, event ):
-    camera = self.scene.GetActiveCamera()
-    
-    y = np.array(camera.GetViewUp())
-    z = np.array(camera.GetPosition()) - np.array(camera.GetFocalPoint())
-    x = np.cross(y, z)
-
-    self.axis_vecs = np.array([x,y,-z])
-    for i,v in enumerate(self.axis_vecs):
-        self.axis_vecs[i] /= np.linalg.norm(self.axis_vecs[i])
-    centers = self.axis_endpoint_positions()
-    for i,c in enumerate(self.axis_vecs):
-      self.axis_eles[i].center = centers[i]
-      self.axis_lines[i].p2 = centers[i]
-
-
-  def start_crystal_view ( self ):
-    from fury import pick
-
-    self.picker = pick.PickingManager()
-    self.atoms.AddObserver('LeftButtonPressEvent', self.left_click, 1)
-    if self.axes:
-      self.smanager.iren.AddObserver('InteractionEvent', self.update_axes)
-    self.smanager.start()
-
-
   def render_atomic_model ( self, model, nsc=(1,1,1), bond_type='stick' ):
     '''
     '''
@@ -151,13 +172,3 @@ class XtraCrysPy:
       self.scene.add(tbond)
       self.bonds.append(tbond)
 
-
-  def show_axes ( self, show=True ):
-    '''
-    '''
-    return
-    from fury import actor
-
-    self.axes = actor.axes()
-    self.scene.add(self.axes)
-    self.scene.reset_camera()
