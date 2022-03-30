0e47688fd50e3bba9ebd574eef7547a3c7b3020d
Frank Cerasoli <ftcerasoli@ucdavis.edu>
2022-03-30 10:00:46 -0700

    Updated defaults with more colors. Modified formatting of examples a tad.

--- examples/CrystalView/example_charge_density.py
+++ examples/CrystalView/example_charge_density.py
@@ -6,7 +6,7 @@ if '__main__' == __name__:
   a_info = {'colors':{'Si':(0,.2,1)}, 'bonds':6.5}
 
   fname = 'data_files/Si.scf.in'
-  xcp = XCP_Atoms.XCP_Atoms(model=fname, params=a_info, nsc=(2,1,1))
+  xcp = XCP_Atoms.XCP_Atoms(model=fname, params=a_info, nsc=(3,3,2))
 
   density_file = 'data_files/Si.density_0.xsf'
   data = read_datablocks_XSF(density_file)
--- examples/CrystalView/example_relax.py
+++ examples/CrystalView/example_relax.py
@@ -2,7 +2,10 @@ from XtraCrysPy import XCP_Atoms as XCP
 
 if '__main__' == __name__:
 
-  a_info = {'colors':{'Ca':(0,1,0),'Al':(0,0,1),'P':(1,0,0)}, 'bonds':5}
+  a_info = {'colors':{'Ca':(0,1,0),
+                      'Al':(0,0,1),
+                      'P':(1,0,0)},
+            'bonds':5}
   # Relaxation
   xcp = XCP.XCP_Atoms(model='data_files/CaAlP.relax.out', 
                       params=a_info, relax=True, sel_type='Chain')
