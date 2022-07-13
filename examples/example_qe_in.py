4d89ad2791fcf70211f0515051a96ff92010071f
Frank Cerasoli <ftcerasoli@ucdavis.edu>
2022-07-13 09:21:32 -0700

    Allow pwi and pwo as extensions for QE files

--- examples/example_qe_in.py
+++ examples/example_qe_in.py
@@ -3,7 +3,7 @@ from XtraCrysPy.Atomic import Atomic
 if '__main__' == __name__:
 
   a_info = {'bonds':3}
-  fname = 'data_files/Si46.scf.in'
+  fname = 'data_files/Si46.scf.pwi'
   xcp = Atomic(params=a_info, model=fname,
                constrain_atoms=True, sel_type='Chain')
   xcp.start_crystal_view()
